af8b6f18903594df9b6345092edf7ff8dea0c4f0
danilgalafeev-svg <danilgalafeev@gmail.com>
2026-02-10 14:47:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 /* 🔥 ВСТАВЬ СЮДА СВОЙ firebaseConfig */
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCQbqSyJqVuKFo_5fsC0vXDYyg96TnHIN0",
